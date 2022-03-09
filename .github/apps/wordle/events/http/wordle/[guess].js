7f13f2559722cf61e8e769c54a5f851868976978
Jeff McAffer <jeffmcaffer@github.com>
2022-03-09 06:16:05 +0000

    Adopt new app model

--- .github/apps/wordle/events/http/wordle/[guess].js
+++ .github/apps/wordle/events/http/wordle/[guess].js
@@ -1,12 +1,9 @@
-let helpers
-let log
+import { keyValue, octokit, _ } from '@extendohub/runtime'
 
 const duration = 24 * 60 * 60 * 1000   // 24 hours in milliseconds
 
-export default async (params) => {
-  helpers = params.helpers
-  log = params.log
-  const { http } = params.events
+export default async ({ events }) => {
+  const { http } = events
   const handler = handlers[http.request.method]
   return handler ? handler(http) : http.response({ status: 404 })
 }
@@ -24,7 +21,7 @@ const handlers = {
   },
   get: async (http) => {
     const { response, sender } = http
-    const game = await helpers.keyValue.get(getKey(sender))
+    const game = await keyValue.get(getKey(sender))
     if (!game || !isToday(game.date)) return response({ status: 404 })
     return cleanGame(game)
   }
@@ -33,7 +30,7 @@ const handlers = {
 async function validateGuess(guess) {
   // The definition API comes back with an array of definitions if there are some and an object if not.
   // TODO right now this API appears to be having performance issues. Need to look for something different.
-  // const definition = await helpers.got(`https://api.dictionaryapi.dev/api/v2/entries/en/${guess}`).json()
+  // const definition = await got(`https://api.dictionaryapi.dev/api/v2/entries/en/${guess}`).json()
   const definition = []
   return guess.length === 5 && Array.isArray(definition)
 }
@@ -65,13 +62,13 @@ function compare(word, guess) {
 }
 
 async function getGame(player) {
-  const game = await helpers.keyValue.get(getKey(player))
+  const game = await keyValue.get(getKey(player))
   if (game && game.word && isToday(game.date)) return game
   return { player, date: today(), status: 'running', word: await getWord(), guesses: [] }
 }
 
 async function saveGame(game) {
-  return helpers.keyValue.set(getKey(game.player), game)
+  return keyValue.set(getKey(game.player), game)
 }
 
 function getKey(player) {
@@ -79,23 +76,23 @@ function getKey(player) {
 }
 
 async function getWord() {
-  const word = await helpers.keyValue.get('word')
+  const word = await keyValue.get('word')
   if (word && isToday(word.date)) return word.word
   const newWord = pickNewWord()
-  await helpers.keyValue.set('word', { word: newWord, date: today() })
+  await keyValue.set('word', { word: newWord, date: today() })
   return newWord
 }
 
 async function pickNewWord() {
   const words = await getWords()
-  const word = words[helpers._.random(0, words.length)]
+  const word = words[_.random(0, words.length)]
   if (!word) throw new Error(`Couldn't pick a word`)
-  await helpers.keyValue.set('word', { word, date: today() })
+  await keyValue.set('word', { word, date: today() })
   return word
 }
 
 async function getWords() {
-  const response = await helpers.octokit.repos.getContent({ owner: 'extendohub', repo: 'gh-wordle', path: 'words.json' })
+  const response = await octokit.repos.getContent({ owner: 'extendohub', repo: 'gh-wordle', path: 'words.json' })
   if (response.statusCode < 200 || response.statusCode >= 300) throw new Error('There was a problem loading words!')
   return JSON.parse(Buffer.from(response.data.content, 'base64').toString('utf8'))
 }
